12779823536151a88c67042542d7c068c53543e6
Kevin CaKuscaasnte Contreras <kecaco_06@hotmail.com>
2019-02-05 13:39:54 -0600

    First Commit

---
+++ FormMultiInsert/FormMultiInsert/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace FormMultiInsert
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ FormMultiInsert/FormMultiInsert/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FormMultiInsert.MvcApplication" Language="C#" %>
